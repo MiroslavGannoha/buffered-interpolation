b15079265df020703c424628152883f21716f447
miro <miro@fluxon.com>
2023-04-11 17:37:49 +0300

    dist update

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,4 @@
-import { Vector3, Quaternion } from "@babylonjs/core/Maths/math.vector";
+import { Vector3, Quaternion } from "@babylonjs/core";
 export declare enum BufferState {
     INITIALIZING = 0,
     BUFFERING = 1,
